0781a7234844138dfa0d3add5107ed698b47f8b6
adawag-jef <epyadawag@gmail.com>
2021-08-15 11:18:55 +0800

    chore:  save new user to database

---
+++ server/controllers/authCtrl.ts
@@ -0,0 +1,35 @@
+import e, { Request, Response } from "express";
+import User from "../models/userModel";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const authCtrl = {
+  register: async (req: Request, res: Response) => {
+    try {
+      const { name, account, password } = req.body;
+
+      // chec if user already registered
+      const user = await User.findOne({ account });
+
+      if (user) {
+        return res.status(400).json({ msg: "Email or Phone already exists." });
+      }
+
+      // hash Password
+      const passwordHash = await bcrypt.hash(password, 12);
+
+      // create new User
+      const newUser = new User({
+        name,
+        account,
+        password: passwordHash,
+      });
+
+      res.json({ msg: "Register successfully", data: newUser });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+};
+
+export default authCtrl;
