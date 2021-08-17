5c79ff37a9a8e7ce8e46d9e6ceb7e5f4edddcf4c
adawag-jef <epyadawag@gmail.com>
2021-08-17 13:31:04 +0800

    chore: send verification sms

--- server/controllers/authCtrl.ts
+++ server/controllers/authCtrl.ts
@@ -4,7 +4,8 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { generateActiveToken } from "../config/generateToken";
 import sendEmail from "../config/sendMail";
-import { validateEmail } from "../middleware/valid";
+import { validateEmail, validPhone } from "../middleware/valid";
+import { sendSms } from "../config/sendSMS";
 
 const CLIENT_URL = process.env.BASE_URL;
 
@@ -36,6 +37,11 @@ const authCtrl = {
         const url = `${CLIENT_URL}/active/${active_token}`;
         sendEmail(account, url, "Verify your email address");
         res.json({ msg: "Success Please check your email." });
+      } else if (validPhone(account)) {
+        const url = `${CLIENT_URL}/active/${active_token}`;
+
+        sendSms(account, url, "Verify your phone number.");
+        res.json({ msg: "Success Please check your phone." });
       }
     } catch (err) {
       return res.status(500).json({ msg: err.message });
