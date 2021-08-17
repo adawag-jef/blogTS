3438796fd2adc42f923e2398e3bb41b504ce819f
Jeff Adawag <68422699+adawag-jef@users.noreply.github.com>
2021-08-17 10:32:55 +0800

    Dev (#2)
    * chore: mailer service
    
    * chore: consolidate error messages to array

--- server/middleware/valid.ts
+++ server/middleware/valid.ts
@@ -6,45 +6,43 @@ export const validRegister = async (
   next: NextFunction
 ) => {
   const { name, account, password } = req.body;
+  const errors = [];
 
   //name is required
   if (!name) {
-    return res.status(400).json({ msg: "Please add your name." });
+    errors.push("Please add your name.");
   }
   //max length
   else if (name.length > 20) {
-    return res.status(400).json({ msg: "Your name is up to 20 chars long." });
+    errors.push("Your name is up to 20 chars long.");
   }
 
   //email or phone number is required
   if (!account) {
-    return res
-      .status(400)
-      .json({ msg: "Please add your email or phone number." });
+    errors.push("Please add your email or phone number.");
   }
   //validate email or phone number format
   else if (!validPhone(account) && !validateEmail(account)) {
-    return res
-      .status(400)
-      .json({ msg: "Email or phone number format is incorrect." });
+    errors.push("Email or phone number format is incorrect.");
   }
 
   //min of 6 characters
   if (password.length < 6) {
-    return res
-      .status(400)
-      .json({ msg: "Password must be atleast 4 characters." });
+    errors.push("Password must be atleast 4 characters.");
   }
 
+  if (errors.length > 0) {
+    return res.status(400).json({ msg: errors });
+  }
   next();
 };
 
-function validPhone(phone: string) {
+export function validPhone(phone: string) {
   const re = /^[+]/g;
   return re.test(phone);
 }
 
-function validateEmail(email: string) {
+export function validateEmail(email: string) {
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
