68a4da8f25d09c578de279109b322b170472bc08
adawag-jef <epyadawag@gmail.com>
2021-08-18 17:36:09 +0800

    chore: login and logout with refresh route

--- server/controllers/authCtrl.ts
+++ server/controllers/authCtrl.ts
@@ -1,8 +1,12 @@
 import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import { generateActiveToken } from "../config/generateToken";
-import { IDecodedToken } from "../config/interface";
+import {
+  generateAccessToken,
+  generateActiveToken,
+  generateRefreshToken,
+} from "../config/generateToken";
+import { IDecodedToken, IUser } from "../config/interface";
 import sendEmail from "../config/sendMail";
 import { sendSms } from "../config/sendSMS";
 import { validateEmail, validPhone } from "../middleware/valid";
@@ -78,6 +82,78 @@ const authCtrl = {
       return res.status(500).json({ msg: errMsg });
     }
   },
+  login: async (req: Request, res: Response) => {
+    try {
+      const { account, password } = req.body;
+
+      const user = await User.findOne({ account });
+
+      if (!user) {
+        return res.status(400).json({ msg: "This account does not exists" });
+      }
+
+      loginUser(user, password, res);
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  logout: async (req: Request, res: Response) => {
+    try {
+      res.clearCookie("refreshToken", { path: "/api/refresh_token" });
+      return res.json({ msg: "Logged Out" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  refreshToken: async (req: Request, res: Response) => {
+    try {
+      const rf_token = req.cookies.refreshToken;
+      if (!rf_token) {
+        return res.status(400).json({ msg: "Please login now" });
+      }
+      const decoded = <IDecodedToken>(
+        jwt.verify(rf_token, `${process.env.REFRESH_TOKEN_SECRET}`)
+      );
+      if (!decoded.id) {
+        return res.status(400).json({ msg: "Please login now" });
+      }
+
+      const user = await User.findById(decoded.id).select("-password");
+
+      if (!user) {
+        return res.status(400).json({ msg: "This account does not exists." });
+      }
+
+      const access_token = generateAccessToken({ id: user._id });
+
+      res.json({ access_token });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+};
+
+const loginUser = async (user: IUser, password: string, res: Response) => {
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return res.status(500).json({ msg: "Password is incorrect" });
+  }
+
+  const access_token = generateAccessToken({ id: user._id });
+  const refresh_token = generateRefreshToken({ id: user._id });
+
+  res.cookie("refreshToken", refresh_token, {
+    httpOnly: true,
+    path: "/api/refresh_token",
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  });
+
+  res.json({
+    msg: "Login Success",
+    access_token,
+    user: { ...user._doc, password: "" },
+  });
 };
 
 export default authCtrl;
--- server/models/userModel.ts
+++ server/models/userModel.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { IUser } from "../config/interface";
 
 const userSchema = new mongoose.Schema(
   {
@@ -30,10 +31,10 @@ const userSchema = new mongoose.Schema(
     },
     type: {
       type: String,
-      default: "normal",
+      default: "register",
     },
   },
   { timestamps: true }
 );
 
-export default mongoose.model("User", userSchema);
+export default mongoose.model<IUser>("User", userSchema);
