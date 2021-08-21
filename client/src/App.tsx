ffe0681659cacce462b23d28db8e2578e5cf8169
adawag-jef <epyadawag@gmail.com>
2021-08-21 18:50:29 +0800

    chore: initial home layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Footer from "./components/global/Footer";
+import Header from "./components/global/Header";
 import PageRender from "./PageRender";
 
 const App = () => {
   return (
     <div className="container">
       <Router>
+        <Header />
         <Switch>
           <Route exact path="/" component={PageRender} />
           <Route exact path="/:page" component={PageRender} />
           <Route exact path="/:page/:slug" component={PageRender} />
         </Switch>
+        <Footer />
       </Router>
     </div>
   );
