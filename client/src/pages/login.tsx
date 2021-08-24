ae71a02664be14c326a4083920e4ff77adc772ac
adawag-jef <epyadawag@gmail.com>
2021-08-24 13:28:31 +0800

    chore: initial login form

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import LoginPass from "../components/auth/LoginPass";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login page</h1>
+    <div className="auth_page">
+      <div className="auth_box">
+        <h3 className="text-uppercase text-center mb-4">Login</h3>
+        <LoginPass />
+      </div>
     </div>
   );
 };
--- client/src/utils/interfaces.ts
+++ client/src/utils/interfaces.ts
@@ -1,4 +1,8 @@
+import React from 'react'
+
 export interface IParams {
   page: string;
   slug: string;
 }
+
+export type InputChange =  React.ChangeEvent<HTMLInputElement> 
